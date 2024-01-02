4475874289d3265729b96709e1fcc593d03a25b9
Nicolás Gómez García <nicogomezgarcia1@gmail.com>
2024-01-02 19:21:25 +0100

    ej 4 apartado 3

---
+++ ejerciciosParte1/Folder0043.js
@@ -0,0 +1,12 @@
+// obtiene todos los elementos <li>
+let listaAnimales = document.querySelectorAll('#lista-animales li');
+
+// Recorre cada elemento y muestra el texto y el número de <li> anidados
+listaAnimales.forEach(function (item) {
+    // Muestra el texto dentro del <li>
+    console.log('Texto dentro del <li>:', item.firstChild.nodeValue.trim());
+
+    // Contar el número de <li> anidados
+    let contador = item.querySelectorAll('li').length;
+    console.log('Número de <li> anidados:', contador);
+});
