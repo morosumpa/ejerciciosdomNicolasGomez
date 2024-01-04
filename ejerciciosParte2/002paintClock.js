fd176af8eb37c894906358c62e34ac2e459d5afe
Nicolás Gómez García <nicogomezgarcia1@gmail.com>
2024-01-04 21:03:19 +0100

    ej 2 parte 2 dom reloj de color que paraq y comienza con event listener

---
+++ ejerciciosParte2/002paintClock.js
@@ -0,0 +1,23 @@
+let intervaloId;
+
+function updateClock() {
+    let horaActual = new Date();
+    let hora = horaActual.getHours().toString().padStart(2, '0');//pasStart para cadenas de texto con un tamaño en especifico
+    let min = horaActual.getMinutes().toString().padStart(2, '0');
+    let secs = horaActual.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById("hours").textContent = hora;
+    document.getElementById("minutes").textContent = min;
+    document.getElementById("seconds").textContent = secs;
+}
+//event listener para los botones strat de que empiece y stop para que pare
+document.getElementById("startBtn").addEventListener("click", function() {
+    intervaloId = setInterval(updateClock, 1000);
+});
+
+document.getElementById("stopBtn").addEventListener("click", function() {
+    clearInterval(intervaloId);
+});
+
+updateClock();
+
