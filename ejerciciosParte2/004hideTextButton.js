9d49513c95002dcf8f4d1a3839557fd3a8cc7c47
Nicolás Gómez García <nicogomezgarcia1@gmail.com>
2024-01-05 18:49:51 +0100

    ej 4 parte 2 dom

---
+++ ejerciciosParte2/004hideTextButton.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {//buscando en internet es obligatorio el uso de DOMContentLoaded ya que es lo que permite que el txt desaparezca
+    let hideButton = document.getElementById('hideButton');
+    let textDiv = document.getElementById('text');
+
+    // addEventListener para cuando lo pulse haga la desaparicion
+    hideButton.addEventListener('click', function () {
+        // Oculta el div al darle al boton
+        textDiv.style.display = 'none';
+    });
+});
